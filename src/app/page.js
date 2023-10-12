6aa5f9b9e11b536c459c063f281210f961b16bc2
William Burroughs <williamburroughs@Williams-MacBook-Pro.local>
2023-10-12 18:21:57 -0400

    Using axios now

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,20 @@
 "use client";
 import React, { useState } from "react";
+import axios from "axios";
 import Image from "next/image";
 import styles from "./page.module.css";
 import Htag from "@/components/atoms/Htag";
 
 export default function Home() {
+
+  let getAxios = () => {
+    axios.get("https://www.jsonkeeper.com/b/MDXW").then(response => {
+      console.log(response)
+    })
+  }
+
+  getAxios()
+
   return (
     <>
       <main className={styles.main}>
